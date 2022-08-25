be2dce2913bd25a205b361046b97455b7d8f3ad2
sickbear <sickbear@mail.ru>
2022-08-25 18:53:52 +0300

    Fix sticky container

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,28 @@
 import '../styles/Sidebar.scss'
-import Sticky from "react-sticky-el";
+import StickyBox from 'react-sticky-box'
 import Profile from './Profile'
 import Suggestion from './Suggestion'
 import Footer from './Footer'
 import image from '../images/avatar.jpg'
 
 function Sidebar() {
+
   return (
-    <Sticky topOffset={ -80 }>
-      <div className="sidebar">
-        <Profile 
-          username="e.suloev" 
-          caption="Eugene Suloev" 
-          urlText="Switch" 
-          iconSize="big" 
-          image={ image } 
-        />
-        <Suggestion />
-        <Footer />
-      </div>
-    </Sticky>
+    <div style={{display: "flex", alignItems: "flex-start"}}>
+      <StickyBox offsetTop={ 60 }>
+        <div className="sidebar">
+            <Profile 
+              username="e.suloev" 
+              caption="Eugene Suloev" 
+              urlText="Switch" 
+              iconSize="big" 
+              image={ image } 
+            />
+          <Suggestion />
+          <Footer />
+        </div>
+      </StickyBox>
+    </div>
   )
 }
 
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,18 +1,10 @@
 import '../styles/Stories.scss'
+
 import Story from './Story'
 
 function Stories() {
   return (
     <div className="stories">
-      {/* TODO: прокрутка */}
-      <Story />
-      <Story />
-      <Story />
-      <Story />
-      <Story />
-      <Story />
-      <Story />
-      <Story />
       <Story />
     </div>
   )
