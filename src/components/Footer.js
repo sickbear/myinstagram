fa3f4f311f6f3be0340949b3d13bc8b5aef27f50
sickbear <sickbear@mail.ru>
2022-08-25 14:59:16 +0300

    Create footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,22 @@ import '../styles/Footer.scss'
 
 function Footer() {
   return (
-    <div className="footer"></div>
+    <div className="footer">
+      <ul className="footer__links">
+        <li>About</li>
+        <li>Help</li>
+        <li>Press</li>
+        <li>API</li>
+        <li>Jobs</li>
+        <li>Privacy</li>
+        <li>Terms</li>
+        <li>Locations</li>
+        <li>Top Accounts</li>
+        <li>Hashtags</li>
+        <li>Language</li>
+      </ul>
+      <div className="footer__copyright">@ 2022 MYINSTAGRAM</div>
+    </div>
   )
 }
 
